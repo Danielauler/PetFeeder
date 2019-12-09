370e7f815fa7e776cd161b4184dcc9acd639c350
Danielauler <daniel-auler@hotmail.com>
2019-12-09 19:46:37 -0300

    com cache

--- projetoFinal/main.cpp
+++ projetoFinal/main.cpp
@@ -6,6 +6,7 @@
 #include <cstdlib>
 #include <tgbot/tgbot.h>
 #include <unistd.h>
+#include <wiringPi.h>
 #include <wiringPiSPI.h>
 #include "scanBowl.h"
 
@@ -43,8 +44,18 @@ int main()
     const string photoFilePath = "foto_img.jpg";
     const string photoMimeType = "image/jpeg";
     int N = 40;
-    wiringPiSetup();
-
+    if (wiringPiSetup() == -1)
+    {
+        puts("Erro em wiringPiSetup().");
+        return -1;
+    }
+    spi_fd = wiringPiSPISetup(0, 500000);
+    if (spi_fd == -1)
+    {
+        puts("Erro abrindo a SPI. Garanta que ela nao");
+        puts("esteja sendo usada por outra aplicacao.");
+        return -1;
+    }
     Bot bot("792286575:AAFZp72JW32M5o7vOafWiOnqqjdN7ca2BIA");
 
     InlineKeyboardMarkup::Ptr keyboard(new InlineKeyboardMarkup);
@@ -65,12 +76,11 @@ int main()
     checkButton2->callbackData = "agendar";
     row1.push_back(checkButton2);
     keyboard->inlineKeyboard.push_back(row1);
-  
+
     checkButton3->text = "Alimentar sem verificar";
     checkButton3->callbackData = "semVerificarAlimentar";
     row2.push_back(checkButton3);
     keyboard->inlineKeyboard.push_back(row2);
-
 
     bot.getEvents().onCommand("start", [&bot](Message::Ptr message) {
         bot.getApi().sendMessage(message->chat->id, "Olá, vou te ajudar a manter seu pet alimentado. Use o comando /help para mais informações");
@@ -81,7 +91,7 @@ int main()
         // feeder.join();
         bot.getApi().sendMessage(message->chat->id, "Alimentado");
     });
-    
+
     bot.getEvents().onCommand("semVerificarAlimentar", [&bot](Message::Ptr message) {
         // thread feeder(feederFunction, 2, 40);
         // feeder.join();
@@ -105,17 +115,18 @@ int main()
         }
     });
 
-     bot.getEvents().onAnyMessage([&bot](TgBot::Message::Ptr message) {
+    bot.getEvents().onAnyMessage([&bot](TgBot::Message::Ptr message) {
         printf("User wrote %s\n", message->text.c_str());
-        if (StringTools::startsWith(message->text, "/start")) {
+        if (StringTools::startsWith(message->text, "/start"))
+        {
             return;
         }
-        const char* send_msp430;
+        unsigned char user_input=1, send_msp430;
         // const unsigned char* user_input = reinterpret_cast<const unsigned char *>(message->text.c_str());
-        const char* user_input = message->text.c_str();
-        if((strcmp(user_input,"0")==0) || (strcmp(user_input,"5")==0))
-			puts("Valor invalido");
-		else 
+        user_input = message->text.c_str();
+        if ((user_input < 0) || (user_input > 5))
+            puts("Valor invalido");
+        else if(user_input>0)
 		{
 			send_msp430 = user_input;
 			wiringPiSPIDataRW(0, &send_msp430, 1);
@@ -146,7 +157,6 @@ int main()
             }
         }
     });
-
 
     bot.getEvents().onCallbackQuery([&bot](CallbackQuery::Ptr query) {
         if (StringTools::startsWith(query->data, "cancel"))
