ac887a651791d39344b79695f918c170c6d8030c
Danielauler <daniel-auler@hotmail.com>
2019-12-09 19:27:15 -0300

    com cache

--- projetoFinal/main.cpp
+++ projetoFinal/main.cpp
@@ -112,7 +112,7 @@ int main()
         }
         unsigned char send_msp430;
         // const unsigned char* user_input = reinterpret_cast<const unsigned char *>(message->text.c_str());
-        unsigned char user_input = message->text;
+        const unsigned char* user_input = message->text.c_str();
         if((strcmp(user_input,"0")==0) || (strcmp(user_input,"5")==0))
 			puts("Valor invalido");
 		else 
