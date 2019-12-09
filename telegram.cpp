8dde8703dfbe2e5abcdc59431c0928d199871ba1
Danielauler <daniel-auler@hotmail.com>
2019-12-09 18:02:42 -0300

    retiradas as função não usadas

--- telegram.cpp
+++ telegram.cpp
@@ -15,27 +15,6 @@ using namespace std;
 using namespace TgBot;
 int spi_fd;
 
-void sqwv(int pin, int degree, int N)
-{
-    int t1 = (100 * degree + 4) / 9 + 1500;
-    int t2 = 20000 - t1;
-    int i;
-    for (i = 0; i < N; i++)
-    {
-        digitalWrite(pin, HIGH);
-        usleep(t1);
-        digitalWrite(pin, LOW);
-        usleep(t2);
-    }
-}
-
-void feederFunction(int delayTime, int N)
-{
-    sqwv(SERVO, 90, N);
-    sleep(delayTime);
-    sqwv(SERVO, 0, N);
-};
-
 int main()
 {
     int N = 40;
@@ -52,8 +31,6 @@ int main()
 		return -1;
 	}
     
-    pinMode(SERVO, OUTPUT);
-    sqwv(SERVO, 0, N);
     Bot bot("931015860:AAHG6qZTMlopgG29lXC6-_rAPSmNrKiYXm4");
 
     InlineKeyboardMarkup::Ptr keyboard(new InlineKeyboardMarkup);
@@ -64,40 +41,17 @@ int main()
     checkButton->callbackData = "alimentar";
     row0.push_back(checkButton);
     keyboard->inlineKeyboard.push_back(row0);
-    vector<InlineKeyboardButton::Ptr> row1;
-    checkButton2->text = "agendar uma refeição";
-    checkButton2->callbackData = "agendar";
-    row1.push_back(checkButton2);
-    keyboard->inlineKeyboard.push_back(row1);
 
     bot.getEvents().onCommand("start", [&bot](Message::Ptr message) {
         bot.getApi().sendMessage(message->chat->id, "Olá, vou te ajudar a manter seu pet alimentado. Use o comando /help para mais informações");
     });
 
     bot.getEvents().onCommand("alimentar", [&bot](Message::Ptr message) {
-        thread feeder(feederFunction, 2, 40);
-        feeder.join();
+        // thread feeder(feederFunction, 2, 40);
+        // feeder.join();
         bot.getApi().sendMessage(message->chat->id, "Alimentado");
     });
 
-    bot.getEvents().onCommand("agendar", [&bot, &keyboard](Message::Ptr message) {
-        string response = "ok";
-        bot.getApi().sendMessage(message->chat->id, response, false, 0, keyboard, "Markdown");
-    });
-
-    bot.getEvents().onCommand("help", [&bot, &keyboard](Message::Ptr message) {
-        bot.getApi().sendMessage(message->chat->id, "Você pode: ", false, 0, keyboard);
-    });
-
-    bot.getEvents().onCallbackQuery([&bot, &keyboard](CallbackQuery::Ptr query) {
-        if (StringTools::startsWith(query->data, "alimentar"))
-        {
-            thread feeder(feederFunction, 2, 40);
-            feeder.join();
-            string response = "Ok, alimentado";
-            bot.getApi().sendMessage(query->message->chat->id, response);
-        }
-    });
     bot.getEvents().onAnyMessage([&bot](TgBot::Message::Ptr message) {
         printf("User wrote %s\n", message->text.c_str());
         if (StringTools::startsWith(message->text, "/start")) {
