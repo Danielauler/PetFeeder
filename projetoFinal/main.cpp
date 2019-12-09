d903ad13b83b07168cb89f0a81f63f9616a9b519
Danielauler <daniel-auler@hotmail.com>
2019-12-09 20:13:21 -0300

    com cache

--- projetoFinal/main.cpp
+++ projetoFinal/main.cpp
@@ -122,9 +122,9 @@ int main()
         {
             return;
         }
-        const unsigned char * user_input;
+        int user_input;
         // const unsigned char* user_input = reinterpret_cast<const unsigned char *>(message->text.c_str());
-        user_input = reinterpret_cast<const unsigned char *> (message->text.c_str());
+        user_input = stoi(message->text.c_str());
         if ((user_input < 0) || (user_input > 5))
             puts("Valor invalido");
         else if(user_input>0)
