4a572b84ac3a27a205af0318be45ac07e4d2c891
Danielauler <daniel-auler@hotmail.com>
2019-12-10 02:15:33 -0300

    change code

--- projetoFinal/main.cpp
+++ projetoFinal/main.cpp
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <iostream>
+#include <string>
 #include <signal.h>
-#include <unistd.h>
 #include <thread>
 #include <tgbot/tgbot.h>
 #include <unistd.h>
