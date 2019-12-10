0640401f31deb8b008e5ffa53cfa28bdf5911d92
Danielauler <daniel-auler@hotmail.com>
2019-12-10 02:20:55 -0300

    change code

--- projetoFinal/main.cpp
+++ projetoFinal/main.cpp
@@ -146,7 +146,7 @@ int main()
         if (StringTools::startsWith(query->data, "alimentar"))
         {
             unsigned char user_input;
-            string value = query->data.c_str().substr(query->data.c_str().find(' '), query->data.c_str().find(' ') + 1);
+            string value = query->data.substr(query->data.find(' '), query->data.find(' ') + 1);
             cout<<"value selected is: "<<value<<endl;
             user_input = (unsigned char)atoi(value);
             if ((atoi(value) < 0) || (atoi(value) > 5))
