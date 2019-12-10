09be6a2a98a7d95fb9d18a068050837f4bc1aba0
Danielauler <daniel-auler@hotmail.com>
2019-12-10 19:23:54 -0300

    teste

--- projetoFinal/main.cpp
+++ projetoFinal/main.cpp
@@ -116,7 +116,7 @@ int main()
         else
         {
             string response = "Se quiser que eu complete o pote, basta me dizer o quão cheio ele já está. Ou pode cancelar!";
-            bot.getApi().sendPhoto(message->chat->id, InputFile::fromFile(photoFilePath, photoMimeType), "A tigela ainda está cheia!");
+            // bot.getApi().sendPhoto(message->chat->id, InputFile::fromFile(photoFilePath, photoMimeType), "A tigela ainda está cheia!");
             bot.getApi().sendMessage(message->chat->id, response, false, 0, keyboard, "Markdown");
         }
     });
