95cf592b4a06c717784b2821a6e2630095c41f12
Danielauler <daniel-auler@hotmail.com>
2019-12-11 14:33:02 -0300

    final

--- servo.h
+++
@@ -1,8 +0,0 @@
-#ifndef MOTOR_h    // To make sure you don't declare the function more than once by including the header multiple times.
-#define MOTOR_h
-#include <servo.h>
-
-
-void motor(const char* tempo);
-
-#endif
